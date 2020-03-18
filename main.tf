8b512291f642983eb27e5f4e3dfdd5504f96bcae
Sean Herman <seanherman@gmail.com>
2020-03-17 23:02:31 -0400

    single server

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  version  = "~>2.53"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fc61db8544a617ed"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "terraform-example"
+  }
+}
